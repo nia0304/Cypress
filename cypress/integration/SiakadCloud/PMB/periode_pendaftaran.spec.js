495362c2088e8f82e3e83ccec7b8c5a1acaac46b
Fikrimlyarf <fikrimlyarf@gmail.com>
2021-10-22 09:00:07 +0700

    penambahan jenis program

--- cypress/integration/SiakadCloud/PMB/periode_pendaftaran.spec.js
+++ cypress/integration/SiakadCloud/PMB/periode_pendaftaran.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-describe("Periode Pendaftaran", () => {
+describe("Happy Flow Periode Pendaftaran", () => {
   beforeEach("Login Siakad", () => {
     cy.login();
     cy.modulpmb();
     cy.filterdashboard();
   });
-  it("Penambahan periode pendaftaran", () => {
+  it("Penambahan periode pendaftaran gratis", () => {
     cy.visit("/spmb/list_periode");
     cy.get(".content-header > h1").should(
       "contain",
@@ -105,10 +105,52 @@ describe("Periode Pendaftaran", () => {
   });
   it.only("Penambahan jenis program pada periode berbayar", () => {
     cy.visit("/spmb/list_periode");
-    cy.contains("a", /^2020\/2021 Genap$/).click();
-    cy.get("#accordion > div:nth-child(3) > div.box-header > h4 > a").click();
-    cy.contains("a", /^Gelombang 4$/).click();
+    cy.contains("a", /^2020\/2021 Genap$/)
+      .click()
+      .wait(0);
+    cy.get("[data-testid=collapseidgelombang]").click();
+    cy.contains("a", /^Gelombang 4$/)
+      .click()
+      .wait(0);
+    cy.get(".text-right > .btn-info").click();
+    cy.get("#sidebar-menu-list > :nth-child(2) > a")
+      .should("be.visible")
+      .click();
+    cy.get("h1 > small").should("contain", "Jenis Program");
+    //tambah jenis program IPA
+    cy.get("#i_idjenispilihan").select("IPA");
+    cy.get("#i_jmlpilihan").select("2");
+    cy.get("#i_jmlpilihanwajib").select("2");
+    cy.get("#i_keterangan").type("Automatioan pilihan jenis Program IPA");
+    cy.get("#insert-row-ms > :nth-child(5) > .btn").click();
+    cy.get(".alert").should(
+      "contain",
+      "Penambahan data Jenis Pilihan Prodi berhasil"
+    );
+    //tambah jenis program IPS
+    cy.get("#i_idjenispilihan").select("IPS");
+    cy.get("#i_jmlpilihan").select("2");
+    cy.get("#i_jmlpilihanwajib").select("2");
+    cy.get("#i_keterangan").type("Automatioan pilihan jenis Program IPS");
+    cy.get("#insert-row-ms > :nth-child(5) > .btn").click();
+    cy.get(".alert").should(
+      "contain",
+      "Penambahan data Jenis Pilihan Prodi berhasil"
+    );
+    //tambah jenis program IPC
+    cy.get("#i_idjenispilihan").select("IPC");
+    cy.get("#i_jmlpilihan").select("2");
+    cy.get("#i_jmlpilihanwajib").select("2");
+    cy.get("#i_keterangan").type("Automatioan pilihan jenis Program IPC");
+    cy.get("#insert-row-ms > :nth-child(5) > .btn").click();
+    cy.get(".alert").should(
+      "contain",
+      "Penambahan data Jenis Pilihan Prodi berhasil"
+    );
   });
+});
+
+describe("Scenario gagal periode pendaftaran", () => {
   it("Penambahan periode pendaftaran tanpa isi kolom nama", () => {
     cy.visit("/spmb/list_periode");
     cy.get(".content-header > h1").should(
