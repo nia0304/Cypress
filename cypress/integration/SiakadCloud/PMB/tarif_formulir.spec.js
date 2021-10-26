5c9b80c7d27c0f5443c32fc9d39d422beb38d834
Fikrimlyarf <fikrimlyarf@gmail.com>
2021-10-26 09:21:57 +0700

    penambahan scenario edit tarif formulir

---
+++ cypress/integration/SiakadCloud/PMB/tarif_formulir.spec.js
@@ -0,0 +1,73 @@
+/// <reference types="cypress" />
+
+describe("Tarif Formulir Periode Pendaftaran", () => {
+  beforeEach("Login Siakad", () => {
+    cy.login();
+    cy.modulpmb();
+    cy.filterdashboard();
+  });
+
+  it("Penambahan tarif formulir", () => {
+    cy.visit("/spmb/list_periode");
+    cy.contains("a", /^2020\/2021 Genap$/)
+      .click()
+      .wait(0);
+    cy.get("[data-testid=collapseidgelombang]").click();
+    cy.contains("a", /^Gelombang 4$/)
+      .click()
+      .wait(0);
+    cy.get(".text-right > .btn-info").click();
+    cy.get("#sidebar-menu-list > :nth-child(3) > a")
+      .should("be.visible")
+      .click();
+    //tarif IPA
+    cy.get("#i_idjenispilihan").select("IPA");
+    cy.get("#i_nominaltarif").type("200000");
+    cy.get("#insert-row-ms > :nth-child(4) > .btn").click();
+    cy.get(".alert").should(
+      "contain",
+      "Penambahan data Tarif Formulir berhasil"
+    );
+    //tarif IPS
+    cy.get("#i_idjenispilihan").select("IPS");
+    cy.get("#i_nominaltarif").type("200000");
+    cy.get("#insert-row-ms > :nth-child(4) > .btn").click();
+    cy.get(".alert").should(
+      "contain",
+      "Penambahan data Tarif Formulir berhasil"
+    );
+    //tarif IPC
+    cy.get("#i_idjenispilihan").select("IPC");
+    cy.get("#i_nominaltarif").type("300000");
+    cy.get("#insert-row-ms > :nth-child(4) > .btn").click();
+    cy.get(".alert").should(
+      "contain",
+      "Penambahan data Tarif Formulir berhasil"
+    );
+  });
+
+  it.only("Edit tarif formulir", () => {
+    cy.visit("/spmb/list_periode");
+    cy.contains("a", /^2020\/2021 Genap$/)
+      .click()
+      .wait(0);
+    cy.get("[data-testid=collapseidgelombang]").click();
+    cy.contains("a", /^Gelombang 4$/)
+      .click()
+      .wait(0);
+    cy.get(".text-right > .btn-info").click();
+    cy.get("#sidebar-menu-list > :nth-child(3) > a")
+      .should("be.visible")
+      .click();
+    cy.get(":nth-child(2) > :nth-child(4) > .btn-primary").click();
+    cy.get("#u_nominaltarif").clear().type("250000");
+    cy.get("#u_tgldeadline").clear().type("12-12-2021");
+    cy.get(":nth-child(2) > :nth-child(4) > .btn-success").click();
+    cy.get(".alert").should(
+      "contain",
+      "Pengubahan data Tarif Formulir berhasil"
+    );
+  });
+  it("Duplikasi data tarif formulir");
+  it("Hapus tarif formulir");
+});
