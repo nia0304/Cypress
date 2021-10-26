3db15c5a80f0aa46baa804507eca13e0fba28097
Fikrimlyarf <fikrimlyarf@gmail.com>
2021-10-26 08:54:13 +0700

    penambahan scenario edit, hapus dan duplikat jenis program

--- cypress/integration/SiakadCloud/PMB/list_periode.spec.js
+++ cypress/integration/SiakadCloud/PMB/list_periode.spec.js
@@ -1,47 +1,52 @@
 /// <reference types="cypress" />
 
 describe("Jenis Program Periode Pendaftaran", () => {
-    beforeEach("Login Siakad", () => {
-      cy.userlogin();
-      cy.modulpmb();
-    });
-    
-    it("Filter halaman list periode pendaftaran", () => {
-        cy.visit('/spmb/list_periode')
-        cy.contains("a", /^2020\/2021 Genap$/).click();
-        cy.wait(5000);
-        cy.get("#accordion > div:nth-child(2) > div.box-header > h4").click();
-        cy.contains("a", /^Mandiri$/).click();
-        cy.get("#accordion > div:nth-child(3) > div.box-header > h4").click();
-        cy.contains("a", /^Gelombang 4$/).click();
-        cy.wait(5000);
-        cy.get("#accordion > div:nth-child(4) > div.box-header > h4").click();
-        cy.contains("a", /^Reguler A$/).click();
-        cy.get("#form_list > div > table").getTable().should(tableData => { 
-          cy.log(tableData)
-          cy.fixture('list_periode.json').then((dataFixture) => {
-            cy.get("#form_list > div > table").getTable().should(tableData => {
-              expect(tableData).to.deep.equal(dataFixture)
-            })
-          })
-        });
-    });
-
-    it("Search periode pendaftaran", () => {
-        cy.visit('/spmb/list_periode')
-        cy.get(".col-xs-8 > .input-group > .form-control")
-          .type("PMB REGULER FREE");
-        cy.get(".input-group-btn > .btn-success > .fa").click();
-    });
-
-    it.only("Berhasil Hapus Periode Pendaftaran", () => {
-        cy.visit('/spmb/list_periode');
-        cy.get(".col-xs-8 > .input-group > .form-control")
-          .type("Testing Nomor");
-        cy.get(".input-group-btn > .btn-success > .fa").click()
-        cy.get(".text-right > .btn-danger > .fa").click();
-        cy.get("header").scrollIntoView();
-        cy.get(".modal-footer > .btn-primary").click();
-        cy.get('.alert').should("contain" , "Penghapusan data Periode Pendaftaran berhasil");
-    })
+  beforeEach("Login Siakad", () => {
+    cy.userlogin();
+    cy.modulpmb();
   });
+
+  it("Filter halaman list periode pendaftaran", () => {
+    cy.visit("/spmb/list_periode");
+    cy.contains("a", /^2020\/2021 Genap$/).click();
+    cy.wait(5000);
+    cy.get("#accordion > div:nth-child(2) > div.box-header > h4").click();
+    cy.contains("a", /^Mandiri$/).click();
+    cy.get("#accordion > div:nth-child(3) > div.box-header > h4").click();
+    cy.contains("a", /^Gelombang 4$/).click();
+    cy.wait(5000);
+    cy.get("#accordion > div:nth-child(4) > div.box-header > h4").click();
+    cy.contains("a", /^Reguler A$/).click();
+    cy.get("#form_list > div > table")
+      .getTable()
+      .should((tableData) => {
+        cy.log(tableData);
+        cy.fixture("list_periode.json").then((dataFixture) => {
+          cy.get("#form_list > div > table")
+            .getTable()
+            .should((tableData) => {
+              expect(tableData).to.deep.equal(dataFixture);
+            });
+        });
+      });
+  });
+
+  it("Search periode pendaftaran", () => {
+    cy.visit("/spmb/list_periode");
+    cy.get(".col-xs-8 > .input-group > .form-control").type("PMB REGULER FREE");
+    cy.get(".input-group-btn > .btn-success > .fa").click();
+  });
+
+  it.only("Berhasil Hapus Periode Pendaftaran", () => {
+    cy.visit("/spmb/list_periode");
+    cy.get(".col-xs-8 > .input-group > .form-control").type("Testing Nomor");
+    cy.get(".input-group-btn > .btn-success > .fa").click();
+    cy.get(".text-right > .btn-danger > .fa").click();
+    cy.get("header").scrollIntoView();
+    cy.get(".modal-footer > .btn-primary").click();
+    cy.get(".alert").should(
+      "contain",
+      "Penghapusan data Periode Pendaftaran berhasil"
+    );
+  });
+});
