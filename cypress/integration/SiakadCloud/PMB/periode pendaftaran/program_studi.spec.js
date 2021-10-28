05c900962f7a12ac17f092263c594aa5ce77fb7d
Fikrimlyarf <fikrimlyarf@gmail.com>
2021-10-28 14:25:32 +0700

    (new) scenario program studi periode pendaftaran

---
+++ cypress/integration/SiakadCloud/PMB/periode pendaftaran/program_studi.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe("Program Studi", () => {
+  it("Penambahan program studi");
+  it("Edir Program Studi");
+  it("Hapus program studi");
+  it("Duplikasi data program studi");
+  it("Salin data program studi");
+});
