4f4d21d5192efce7d1a7f78c367a2bf6518c2bba
Fikrimlyarf <fikrimlyarf@gmail.com>
2021-11-01 15:43:12 +0700

    (pending) scenario penambahan program studi

--- cypress/integration/SiakadCloud/PMB/periode pendaftaran/program_studi.spec.js
+++ cypress/integration/SiakadCloud/PMB/periode pendaftaran/program_studi.spec.js
@@ -1,7 +1,39 @@
 /// <reference types="cypress" />
 
 describe("Program Studi", () => {
-  it("Penambahan program studi");
+  beforeEach("Login Siakad", () => {
+    cy.login();
+    cy.modulpmb();
+    cy.filterdashboard();
+  });
+  it.only("Penambahan program studi", () => {
+    cy.visit("/spmb/list_periode");
+    cy.contains("a", /^2020\/2021 Genap$/)
+      .click()
+      .wait(0);
+    cy.get("[data-testid=collapseidgelombang]").click();
+    cy.contains("a", /^Gelombang 4$/)
+      .click()
+      .wait(0);
+    cy.get(".text-right > .btn-info").click();
+    cy.get("#sidebar-menu-list > :nth-child(4) > a")
+      .should("be.visible")
+      .click();
+    cy.get(".col-md-7 > .btn-success").click();
+    cy.get("#idunit").select("S1 - Sistem Informasi");
+    cy.get("#idjenispilihan").select("IPA");
+    cy.get("#jmlditerima").type("150");
+    cy.get("#nilaiminimal").type("75");
+    cy.get("#prefixnim").type("202104");
+    cy.get("#jmlurutannim").select("3");
+    // cy.get(
+    //   "#block-pilihan[1] > .col-md-7 > .labelinput > .icheckbox_minimal > .iCheck-helper"
+    // ).click();
+    // cy.get("#block-pilihan[1] > div > label > div > ins").click();
+    cy.get(
+      "#block-pilihan[1] > div > label > .icheckbox_minimal > .iCheck-helper"
+    ).click();
+  });
   it("Edir Program Studi");
   it("Hapus program studi");
   it("Duplikasi data program studi");
