d7f7dcdb2bc9ac374c237a3dc5dfe361ca4a794c
yasinta <yasintaanggraeni19@gmail.com>
2021-11-08 20:25:43 +0700

    Hapus & Duplikat data penguji seleksi

--- cypress/integration/SiakadCloud/PMB/Jadwal Seleksi/ms_peserta.spec.js
+++ cypress/integration/SiakadCloud/PMB/Jadwal Seleksi/ms_peserta.spec.js
@@ -30,4 +30,24 @@ describe("Peserta Seleksi", () => {
             });
         });
     });
+
+    it.only("Hapus Peserta Seleksi", () => {
+      cy.visit('/spmb/list_jadwal');
+      cy.get("#periode").select("2020/2021 Genap");
+      cy.wait(5000);
+      cy.get("#jalur").select("Mandiri");
+      cy.wait(5000);
+      cy.get("#gelombang").select("Gelombang 1");
+      cy.wait(5000);
+      cy.get("#sistem").select("Reguler A");
+      cy.wait(5000);
+      cy.get(":nth-child(2) > .text-right > .btn-info > .fa").click();
+      cy.get("#sidebar-menu-list > :nth-child(3) > a")
+        .should("contain" , "Peserta")
+        .click();
+      cy.get(".btn-danger > .fa").click();
+      cy.get(".modal-footer > .btn-primary").click();
+      cy.get("header").scrollIntoView();
+      cy.get(".alert").should("contain" , "Pengubahan data Seleksi Pendaftar berhasil");
+  });
 });
