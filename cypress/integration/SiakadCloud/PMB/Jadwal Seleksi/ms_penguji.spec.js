8dd613bf5a895ec6d1d705f4ef844ce7acf09980
yasinta <yasintaanggraeni19@gmail.com>
2021-11-08 19:49:28 +0700

    Edit dan Duplikat Penguji

--- cypress/integration/SiakadCloud/PMB/Jadwal Seleksi/ms_penguji.spec.js
+++ cypress/integration/SiakadCloud/PMB/Jadwal Seleksi/ms_penguji.spec.js
@@ -32,7 +32,7 @@ describe("Data Penguji", () => {
         cy.get(".alert").should("contain" , "Penambahan data Penguji Seleksi berhasil");
     });
 
-    it.only("Edit Data Penguji", () => {
+    it("Edit Data Penguji", () => {
         cy.visit('/spmb/list_jadwal');
         cy.get("#periode").select("2020/2021 Genap");
         cy.wait(5000);
@@ -58,5 +58,51 @@ describe("Data Penguji", () => {
         //cy.get("#insert-row-ms > :nth-child(4) > .btn > .fa").click();
         cy.get("header").scrollIntoView();
         cy.get(".alert").should("contain" , "Pengubahan data Penguji Seleksi berhasil");
+    });
+
+    it("Hapus data penguji", () => {
+        cy.visit('/spmb/list_jadwal');
+        cy.get("#periode").select("2020/2021 Genap");
+        cy.wait(5000);
+        cy.get("#jalur").select("Mandiri");
+        cy.wait(5000);
+        cy.get("#gelombang").select("Gelombang 1");
+        cy.wait(5000);
+        cy.get("#sistem").select("Reguler A");
+        cy.wait(5000);
+        cy.get(":nth-child(2) > .text-right > .btn-info > .fa").click();
+        cy.get("#sidebar-menu-list > :nth-child(2) > a")
+          .should("contain" , "Penguji")
+          .click();
+        cy.get(".btn-danger > .fa").click();
+        cy.get(".modal-footer > .btn-primary").click();
+        cy.get("header").scrollIntoView();
+        cy.get(".alert").should("contain" , "Penghapusan data Penguji Seleksi berhasil");
+    });
+
+      it.only("Duplikat data penguji", () => {
+        cy.visit('/spmb/list_jadwal');
+        cy.get("#periode").select("2020/2021 Genap");
+        cy.wait(5000);
+        cy.get("#jalur").select("Mandiri");
+        cy.wait(5000);
+        cy.get("#gelombang").select("Gelombang 1");
+        cy.wait(5000);
+        cy.get("#sistem").select("Reguler A");
+        cy.wait(5000);
+        cy.get(":nth-child(2) > .text-right > .btn-info > .fa").click();
+        cy.get("#sidebar-menu-list > :nth-child(2) > a")
+          .should("contain" , "Penguji")
+          .click();
+        cy.get('#i_nip_label').type("andr")
+        cy.get('.tt-dataset-1 div').each(($el, index , $list) =>{
+            if($el.text() == '1001079001 - ANDREW SHANDY UTAMA' ) {
+                cy.wrap($el).click()
+            }
+        });
+        cy.get(":nth-child(2) > .labelinput > .icheckbox_minimal > .iCheck-helper").click();
+        cy.get("#insert-row-ms > :nth-child(4) > .btn > .fa").click();
+        cy.get("header").scrollIntoView();
+        cy.get(".alert").should("contain" , "Bentrok Dengan Seleksi TES CBT Tanggal 1 Oktober 2021 Jam 11:00:00 - 14:00:00 ")
     });
 });
