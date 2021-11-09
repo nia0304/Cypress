f352fe20b8354207f2342454150403e7abd51f67
yasinta <yasintaanggraeni19@gmail.com>
2021-11-09 19:59:12 +0700

    [Add] Generate jadwal seleksi

--- cypress/integration/SiakadCloud/PMB/Jadwal Seleksi/list_jadwal.spec.js
+++ cypress/integration/SiakadCloud/PMB/Jadwal Seleksi/list_jadwal.spec.js
@@ -70,7 +70,7 @@ describe("List Jadwal Seleksi", () => {
       cy.get(".alert").should("contain" , "Penghapusan data Jadwal Seleksi berhasil");
     });
 
-    it.only("Duplikat Jadwal Seleksi", () => {
+    it("Duplikat Jadwal Seleksi", () => {
       cy.visit('/spmb/list_jadwal');
       cy.get("#periode").select("2020/2021 Genap");
       cy.wait(5000);
@@ -97,5 +97,38 @@ describe("List Jadwal Seleksi", () => {
         "Ruangan Bentrok Dengan Jadwal TES CBT Pada Ruang Ruang 111  Tanggal 1 Oktober 2021 Jam 11:00:00 - 14:00:00"
       );
       cy.get(".btn-info > span").click();
+  });
+
+    it.only("Generate Jadwal Seleksi", () => {
+      cy.visit('/spmb/list_jadwal');
+      cy.get("#periode").select("2020/2021 Genap");
+      cy.wait(5000);
+      cy.get("#jalur").select("Mandiri");
+      cy.wait(5000);
+      cy.get("#gelombang").select("Gelombang 1");
+      cy.wait(5000);
+      cy.get("#sistem").select("Reguler A");
+      cy.wait(5000);
+      cy.get(".btn-warning").click();
+      cy.get("#idjenispilihan").select("IPA").should("have.value", "IPA");
+      cy.get("#idseleksiprodi").select("KELENGKAPAN").should("have.value", "16");
+      cy.get("#tgljadwal").type("02-10-2021").tab();
+      cy.get("#waktumulai").type("11:00");
+      cy.get("#waktuselesai").type("14:00");
+      cy.get(".col-sm-8 > :nth-child(2)").click();
+      cy.get(".modal-footer > .btn").click();
+      cy.get(".modal-footer > .btn-primary").click();
+      cy.get("header").scrollIntoView();
+      cy.get(".alert").should(
+        "contain",
+        "Penambahan data Jadwal Seleksi berhasil"
+      );
+      cy.get("#form_list > div > table").getTable().should(tableData => { 
+        //        hasil log data diletakkan di fixture          
+                cy.log(tableData)
+        //    cy.fixture('list_periode.json').then((dataFixture) => {
+        //              expect(tableData).to.deep.equal(dataFixture)
+        //    })
+      });
   });
 });
