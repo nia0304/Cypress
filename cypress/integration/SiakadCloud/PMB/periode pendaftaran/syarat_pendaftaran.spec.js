b06b69ee3d0190011730319a438b7dea55adf4ca
Fikrimlyarf <fikrimlyarf@gmail.com>
2021-11-13 13:18:37 +0700

    (new) salin syarat pendaftaran

--- cypress/integration/SiakadCloud/PMB/periode pendaftaran/syarat_pendaftaran.spec.js
+++ cypress/integration/SiakadCloud/PMB/periode pendaftaran/syarat_pendaftaran.spec.js
@@ -56,12 +56,13 @@ describe("Syarat Pendaftaran", () => {
       "Pengubahan data Syarat Pendaftaran berhasil"
     );
   });
-  it.only("Hapus syarat pendaftaran", () => {
+  it("Hapus syarat pendaftaran", () => {
     cy.visit("/spmb/list_periode");
     cy.filterlistperiodependaftaran();
     cy.get("#sidebar-menu-list > :nth-child(7) > a")
       .should("be.visible")
       .click();
+    //klik hapus syarat urutan 1
     cy.get(":nth-child(2) > :nth-child(6) > .btn-danger").click();
     cy.get(".bootbox > .modal-dialog > .modal-content").should(
       "contain",
@@ -73,11 +74,40 @@ describe("Syarat Pendaftaran", () => {
       "contain",
       "Penghapusan data Syarat Pendaftaran berhasil"
     );
+    //klik hapus syarat urutan 2
     cy.get(":nth-child(2) > :nth-child(6) > .btn-danger").click();
     cy.get(".modal-footer > .btn-primary").click();
     cy.get(".alert").should(
       "contain",
       "Penghapusan data Syarat Pendaftaran berhasil"
     );
+  });
+  it.only("Salin syarat pendaftar", () => {
+    cy.visit("/spmb/list_periode");
+    cy.filterlistperiodependaftaran();
+    cy.get("#sidebar-menu-list > :nth-child(7) > a")
+      .should("be.visible")
+      .click();
+    //klik salin
+    cy.get(".col-md-7 > .btn-warning").should("contain", "Salin").click();
+    cy.get("#modal-copy > .modal-dialog > .modal-content")
+      .should("contain", "Salin Sebaran Program Studi")
+      .and("be.visible");
+    //pilih filter salin
+    cy.get("#periodecopy").select("2020/2021 Genap");
+    cy.get("#jalurcopy").select("Mandiri");
+    cy.get("#gelombangcopy").select("Gelombang 3");
+    cy.get("#sistemkuliahcopy").select("Reguler A");
+    cy.get("#syaratcopy").select("Administrasi");
+    cy.get(".modal-footer > .btn").should("be.visible").click();
+    //pop up konfirmasi
+    cy.get(".bootbox > .modal-dialog > .modal-content")
+      .should(
+        "contain",
+        "Apakah anda yakin akan menyalin Syarat Daftar Administrasi?"
+      )
+      .and("be.visible");
+    cy.get(".modal-footer > .btn-default").should("contain", "Cancel");
+    cy.get(".modal-footer > .btn-primary").click();
   });
 });
