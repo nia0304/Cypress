f1f49b2486591352dc7b628d3b59095bd7013c77
yasinta <yasintaanggraeni19@gmail.com>
2021-11-22 12:51:31 +0700

    [Add] Edit nilai seleksi dengan upload file di tabel

--- cypress/integration/SiakadCloud/PMB/Nilai Seleksi/list_nilaiseleksi.spec.js
+++ cypress/integration/SiakadCloud/PMB/Nilai Seleksi/list_nilaiseleksi.spec.js
@@ -24,9 +24,27 @@ describe("List Nilai Seleksi", () => {
         cy.get(":nth-child(3) > :nth-child(7)").click();
         cy.get("#u_keteranganseleksi").type("Benar semua");
         //upload file
-        //const filepath = 'images/minion 3.jpg'
-        cy.get('#form_list > div.table-responsive > table > tbody > tr:nth-child(3) > td:nth-child(9) > input[type=file]');
-          //.attachfile();
-
+        const filepath = 'images/minion 3.jpg'
+        cy.get('#form_list > div.table-responsive > table > tbody > tr:nth-child(3) > td:nth-child(9) > input[type=file]')
+          .attachFile(filepath);
+        cy.get(":nth-child(10) > .btn-success > .fa").click();
     });
+
+    it.only("Import Nilai Seleksi" , () => {
+      cy.visit("/spmb/list_nilaiseleksi");
+      cy.get("#periode").select("2020/2021 Genap");
+      cy.wait(5000);
+      cy.get("#gelombang").select("Gelombang 1");
+      cy.wait(5000);
+      cy.get("#jalur").select("Mandiri");
+      cy.wait(5000);
+      cy.get("#sistem").select("Reguler A");
+      cy.wait(5000);
+      cy.get("#pilihan").select("IPA");
+      cy.wait(5000);
+      cy.get("#seleksi").select("TES CBT");
+      cy.wait(5000);
+      cy.get("#wrap-button > .btn").click();
+      cy.get(":nth-child(9) > .col-md-12 > .btn").click();
+  });
 });
