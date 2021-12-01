a5a4595ad779f4a119083d736640c0a3bf6a88c5
Fikrimlyarf <fikrimlyarf@gmail.com>
2021-12-01 14:43:21 +0700

    [add] download file daftar ulang

---
+++ cypress/integration/Dapur/Download.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-downloadfile"/>
+
+describe('download file', ()=> {
+    beforeEach("Login Siakad", () => {
+        cy.login();
+        cy.modulpmb();
+    });
+    it('download', ()=> {
+        cy.visit("/spmb/set_registrasi");
+        cy.get('#idpendaftar_label').type('khalil')
+        cy.get('.tt-suggestions div').each(($el, index, $list) => {
+			if ($el.text() === '202411001 - KHALIL GIBRAN') {
+			cy.wrap($el).click()
+			}
+		})
+        cy.get('.input-group-btn > .btn').click()
+        cy.downloadFile('http://localhost/siacloud/spmb/../uploads//fileadministrasi/35415_14','Downloads','lorem-ipsum.pdf')
+        
+        const fileName = 'Downloads/lorem-ipsum.pdf'
+        cy.log(fileName) 
+        cy.readFile(fileName)
+    
+    })
+})
