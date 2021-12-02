dc029fe644f676dae206f0d34e83acfb6ef26c9e
yasinta <yasintaanggraeni19@gmail.com>
2021-12-02 19:49:16 +0700

    [Update] Simpan data nilai seleksi

--- cypress/integration/SiakadCloud/PMB/Nilai Seleksi/list_nilaiseleksi.spec.js
+++ cypress/integration/SiakadCloud/PMB/Nilai Seleksi/list_nilaiseleksi.spec.js
@@ -6,7 +6,7 @@ describe("List Nilai Seleksi", () => {
       cy.modulpmb();
     });
 
-    it("Edit Nilai Seleksi" , () => {
+    it.only("Edit Nilai Seleksi" , () => {
         cy.visit("/spmb/list_nilaiseleksi");
         cy.get("#periode").select("2020/2021 Genap");
         cy.wait(5000);
@@ -14,24 +14,24 @@ describe("List Nilai Seleksi", () => {
         cy.wait(5000);
         cy.get("#jalur").select("Mandiri");
         cy.wait(5000);
-        cy.get("#sistem").select("Reguler A");
+        cy.get("#sistem").select("Reguler B");
         cy.wait(5000);
-        cy.get("#pilihan").select("IPA");
+        cy.get("#pilihan").select("IPS");
         cy.wait(5000);
-        cy.get("#seleksi").select("TES CBT");
-        cy.get(":nth-child(9) > :nth-child(10) > .btn > .fa").click();
+        cy.get("#seleksi").select("Tes Kesehatan");
+        cy.get(":nth-child(10) > .btn > .fa").click();
         cy.get("#u_nilaikomposisi0").type("78");
-        cy.get(":nth-child(9) > :nth-child(7)").click();
+        cy.get("tbody > tr > :nth-child(7)").click();
         cy.get("#u_keteranganseleksi").type("Benar semua");
         //upload file
         const filepath = 'images/minion 3.jpg'
-        cy.get('#form_list > div.table-responsive > table > tbody > tr:nth-child(9) > td:nth-child(9) > input[type=file]')
+        cy.get('#form_list > div.table-responsive > table > tbody > tr > td:nth-child(9) > input[type=file]')
           .attachFile(filepath);
-        cy.get("#form_list > div.table-responsive > table > tbody > tr:nth-child(9) > td:nth-child(10) > button.btn.btn-success.btn-xs.btn-flat").click();
+        cy.get("#form_list > div.table-responsive > table > tbody > tr > td:nth-child(10) > button.btn.btn-success.btn-xs.btn-flat").click();
         cy.get(".alert").should("contain","Pengubahan data Keterangan Seleksi Pendaftar berhasil");
     });
 
-    it.only("Import Nilai Seleksi" , () => {
+    it("Import Nilai Seleksi" , () => {
       cy.visit("/spmb/list_nilaiseleksi");
       cy.get("#periode").select("2020/2021 Genap");
       cy.wait(5000);
@@ -39,11 +39,11 @@ describe("List Nilai Seleksi", () => {
       cy.wait(5000);
       cy.get("#jalur").select("Mandiri");
       cy.wait(5000);
-      cy.get("#sistem").select("Reguler A");
+      cy.get("#sistem").select("Reguler B");
       cy.wait(5000);
       cy.get("#pilihan").select("IPA");
       cy.wait(5000);
-      cy.get("#seleksi").select("TES CBT");
+      cy.get("#seleksi").select("TES Kesehatan");
       cy.wait(5000);
       cy.get("#wrap-button > .btn").click();
       //klik download
