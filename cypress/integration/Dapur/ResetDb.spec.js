379d2abfb57bfc342934d416410e238c6e5ca203
Putra Prima Arhandi <putraprima@gmail.com>
2022-01-10 12:43:58 +0700

    [Add] Backup Restore Spmb

--- cypress/integration/Dapur/ResetDb.spec.js
+++ cypress/integration/Dapur/ResetDb.spec.js
@@ -1,11 +1,16 @@
-describe('Tes Reset Database', () => {
-    before(() => {
-        // backup dulu database nya command backup 
-        // pg_dump --schema=schemaname,schemaname --clean dbname > filebackup.sql
-        // taroh file backup di folder fixtures
-       cy.exec('psql -U username -d dbname -f ./cypress/fixtures/filebackup.sql')
-    });
-    it('Login dan Delete User Soko', () => {
-        // test code disini
-    });
+describe("Tes Reset Database", () => {
+  before(() => {
+    // psql -U sevima -d siacloud -c 'DROP Schema spmb cascade'
+
+    // pg_dump --schema=schemaname,schemaname --clean dbname > filebackup.sql
+    // taroh file backup di folder fixtures
+    cy.exec("psql -U sevima -d siacloud -c 'DROP Schema spmb cascade'");
+
+    cy.exec(
+      'pg_restore --clean --if-exist --format=custom --dbname=siacloud "./cypress/fixtures/db/tes.dump"'
+    );
+  });
+  it("Login dan Delete User Soko", () => {
+    // test code disini
+  });
 });
