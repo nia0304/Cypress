1d942faad3b817b1532d4a63b655a5c3800d0418
Fikrimlyarf <fikrimlyarf@gmail.com>
2022-01-10 14:05:36 +0700

    (add) tes resetdb

--- cypress/integration/Dapur/ResetDb.spec.js
+++ cypress/integration/Dapur/ResetDb.spec.js
@@ -1,11 +1,17 @@
-describe('Tes Reset Database', () => {
+describe("Tes Reset Database", () => {
     before(() => {
-        // backup dulu database nya command backup 
-        // pg_dump --schema=schemaname,schemaname --clean dbname > filebackup.sql
-        // taroh file backup di folder fixtures
-       cy.exec('psql -U username -d dbname -f ./cypress/fixtures/filebackup.sql')
+      // psql -U sevima -d siacloud -c 'DROP Schema spmb cascade'
+  
+      // pg_dump --schema=schemaname,schemaname --clean dbname > filebackup.sql
+      // taroh file backup di folder fixtures
+      cy.exec("psql -U sevima -d siacloud -c 'DROP Schema spmb cascade'");
+  
+      cy.exec(
+        'pg_restore --clean --if-exist --format=custom --dbname=siacloud "./cypress/fixtures/db/tes.dump"'
+      );
     });
-    it('Login dan Delete User Soko', () => {
-        // test code disini
+    it("Login dan Delete User Soko", () => {
+      // test code disini
     });
-});
+  });
+  
