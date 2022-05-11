b8033b9320646968cbc58d035e624e9eabda864f
Fikrimlyarf <fikrimlyarf@gmail.com>
2022-05-11 12:22:02 +0700

    [add] fixture konsultasi krs

--- cypress/integration/SiakadCloud/akademik/mahasiswa/konsultasi_krs.spec.js
+++ cypress/integration/SiakadCloud/akademik/mahasiswa/konsultasi_krs.spec.js
@@ -18,7 +18,7 @@ describe("Konsultasi KRS", () => {
         cy.get('.select2-results__option').should("contain", "No results found")
       })
 
-      it.only("Pilih filter periode konsultasi", ()=> {
+      it("Pilih filter periode konsultasi", ()=> {
         cy.get('#select2-select-periode-container').click()
         cy.get('.select2-search__field').type("2020/2021 Genap")
         cy.get('#select2-select-periode-results').each(($el, index, $list)=> {
@@ -26,5 +26,23 @@ describe("Konsultasi KRS", () => {
               cy.wrap($el).click()
           }
       })
+      })
+
+      it("Mengosongkan isian modal tambah  konsultasi", ()=>{
+        cy.get('[data-cy="#modal-btn-konsultasi"] > .ripple').click()
+        cy.get('#modal-add-konsultasi').should('be.visible')
+        cy.get('[data-cy="input-topik"]').type('Konsultasi Pengambilan KRS')
+        cy.get('[data-cy="input-keterangan"]').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+        cy.get('[data-cy="btn-kosongkan"] > .ripple').click()
+      })
+
+      it.only("Menambah konsultasi", ()=>{
+        cy.get('[data-cy="#modal-btn-konsultasi"] > .ripple').click()
+        cy.get('#modal-add-konsultasi').should('be.visible')
+        cy.get('[data-cy="input-topik"]').type('Konsultasi Pengambilan KRS')
+        cy.get('[data-cy="input-keterangan"]').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+        cy.get('[data-cy="btn-mulai-konsultasi"] > .ripple').click()
+        cy.get('.top-alert > .container-xxl').should('contain', 'Penambahan data konsultasi pembimbing akademik berhasil').and('be.visible')
+        cy.get('[data-cy="info-nav-chat"]').should('contain', 'Konsultasi Pengambilan KRS').and('be.visible')
       })
 })
