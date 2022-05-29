3e1f3144e41f6c4c8bc9500e5e4deb2f77fd9861
Fikrimlyarf <fikrimlyarf@gmail.com>
2022-05-29 17:45:23 +0700

    [add] konsultasi bimbingan role dosen

--- cypress/integration/SiakadCloud/akademik/dosen/konsultasi_mhs.spec.js
+++ cypress/integration/SiakadCloud/akademik/dosen/konsultasi_mhs.spec.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress"/>
 
 describe("Konsultasi Mahasiswa", () => {
-  before("create db", () => {
-    cy.exec("createdb -U postgres -p 5433 -T backupdb testsiakad");
-  });
+  // before("create db", () => {
+  //   cy.exec("createdb -U postgres -p 5433 -T backupdb testsiakad");
+  // });
   beforeEach("Login Dosen", () => {
     cy.logindosen();
     cy.modulakademik();
     cy.visit("https://dev.siakadcloud.com/siakad/data_konsultasi_new");
   });
-  after(() => {
-    cy.exec("dropdb -U postgres -p 5433 testsiakad");
-  });
+  // after(() => {
+  //   cy.exec("dropdb -U postgres -p 5433 testsiakad");
+  // });
 
   it("Pilih filter periode konsultasi", () => {
     cy.fixture("akademik/mahasiswa/konsultasi_krs").then((data) => {
@@ -22,6 +22,99 @@ describe("Konsultasi Mahasiswa", () => {
           cy.wrap($el).click();
         }
       });
+      cy.get(
+        '.content-chat > .field-chat > [data-cy="empty-content-konsultasi"] > .empty-state'
+      ).should("contain", "Belum ada Obrolan Disini");
+    });
+  });
+
+  it("Filter periode konsultasi tidak ada", () => {
+    cy.fixture("akademik/mahasiswa/konsultasi_krs").then((data) => {
+      cy.get("#select2-select-periode-container").click();
+      cy.get(".select2-search__field").type(data.periode_salah);
+      cy.get(".select2-results__option").should(
+        "contain",
+        data.info_periodesalah
+      );
+    });
+  });
+
+  it("Kosongkan Kolom Tambah konsultasi", () => {
+    cy.periodekonsultasi();
+    cy.fixture("akademik/mahasiswa/konsultasi_krs").then((data) => {
+      cy.get('[data-cy="#modal-btn-konsultasi"] > .ripple').click();
+      cy.get("#modal-add-konsultasi").should("be.visible");
+      cy.get('[data-cy="data-periode"]').should("have.value", data.periode);
+      cy.get("#select2-select-nama-modal-container").click();
+      cy.get(".select2-search__field").type(data.exnama_mhs);
+      cy.get("#select2-select-nama-modal-results").each(($el, index, $list) => {
+        if ($el.text() === data.nama_mhs) {
+          cy.wrap($el).click();
+        }
+      });
+      cy.get('[data-cy="input-topik"]').type(data.topik1);
+      cy.get('[data-cy="input-keterangan"]').type(data.keterangan);
+      cy.get('[data-cy="btn-kosongkan"] > .ripple').click();
+    });
+  });
+  it("Tambah konsultasi", () => {
+    cy.periodekonsultasi();
+    cy.fixture("akademik/mahasiswa/konsultasi_krs").then((data) => {
+      cy.get('[data-cy="#modal-btn-konsultasi"] > .ripple').click();
+      cy.get("#modal-add-konsultasi").should("be.visible");
+      cy.get('[data-cy="data-periode"]').should("have.value", data.periode);
+      cy.get("#select2-select-nama-modal-container").click();
+      cy.get(".select2-search__field").type(data.exnama_mhs);
+      cy.get("#select2-select-nama-modal-results").each(($el, index, $list) => {
+        if ($el.text() === data.nama_mhs) {
+          cy.wrap($el).click();
+        }
+      });
+      cy.get('[data-cy="input-topik"]').type(data.topik1);
+      cy.get('[data-cy="input-keterangan"]').type(data.keterangan);
+      cy.get('[data-cy="btn-mulai-konsultasi"] > .ripple').click();
+      cy.get(".top-alert > .container-xxl").should(
+        "contain",
+        data.alert_tambahkonsultasi
+      );
+      cy.get(
+        ':nth-child(3) > [data-cy="list-chat-konsultasi"] > .justify-content-between'
+      )
+        .should("contain", data.exnama_mhs)
+        .and("be.visible");
+      cy.get(
+        '[data-cy="list-content-konsultasi-20220204"] > .list-content-konsultasi'
+      )
+        .should("contain", data.topik1)
+        .and("be.visible");
+      cy.get(
+        '.content-chat > .field-chat > [data-cy="empty-content-konsultasi"] > .empty-state'
+      ).should("contain", data.empty_state1);
+    });
+  });
+
+  it.only("Chat konsultasi bimbingan", () => {
+    cy.periodekonsultasi();
+    cy.fixture("akademik/mahasiswa/konsultasi_krs").then((data) => {
+      cy.get(
+        ':nth-child(3) > [data-cy="list-chat-konsultasi"] > .justify-content-between'
+      ).click();
+      cy.get(
+        '[data-cy="list-content-konsultasi-20220204"] > .list-content-konsultasi'
+      ).click();
+      cy.get(".justify-content-between > .d-block > h3").should(
+        "contain",
+        data.topik1
+      );
+      cy.get(
+        '.content-chat > .field-chat > [data-cy="empty-content-konsultasi"] > .empty-state'
+      ).should("contain", data.empty_state1);
+      cy.get('[data-cy="input-chat"]').type(data.chat_dsn);
+      cy.get('[data-cy="btn-submit-chat"]').click();
+      cy.get(".top-alert > .container-xxl")
+        .should(data.alert_chat)
+        .and("be.visible");
+      cy.get(".right > .bubble-chat").should("contain", data.chat_dsn);
     });
   });
 });
