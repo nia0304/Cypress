abd9db359e318fc905f45584678d84d4b753a485
Putra Prima Arhandi <putraprima@gmail.com>
2022-07-22 06:37:18 +0700

    Fixing Login User, Tambah Fitur biar jalan satu folder

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,16 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 800,
   chromeWebSecurity: false,
+  video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost/siacloud',
+    baseUrl: "http://localhost/siacloud",
   },
-})
+});
