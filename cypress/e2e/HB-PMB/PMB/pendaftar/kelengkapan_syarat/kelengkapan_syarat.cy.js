7aade024882ab81dd8e8133df4de0c321b8ad9b8
Tri Meida Ratnasari <trimeida230596@gmail.com>
2022-08-08 09:06:06 +0700

    [ADD kelengkapan syarat administrasi pendaftar

---
+++ cypress/e2e/HB-PMB/PMB/pendaftar/kelengkapan_syarat/kelengkapan_syarat.cy.js
@@ -0,0 +1,49 @@
+describe("Pengisian Biodata", () => {
+
+    beforeEach("login pendaftar", () => {
+        //login
+        cy.loginPendaftar()
+    });
+
+    it("Pengisian biodata pendaftar", () => {
+        //get dan klik href berkas administrasi
+        cy.get('.side-menu > ul > :nth-child(2) > a')
+            .invoke('removeAttr', 'target')
+            .click()
+        cy.get('.main-header').should('contain', 'BERKAS ADMINISTRASI')
+
+        cy.fixture("pmb/data_pendaftar").then((data) => {
+            cy.get('.col-md-6 > .summary-value').should('contain', data.namapendaftar)
+
+
+            // cy.get('#profil-foto').click()
+            const filepath = 'Images/minion 3.jpg'
+            cy.get('#profil-foto').attachFile(filepath)
+
+            cy.get('div[id="fileList-filesyarat[14]"]').click({ force: true })
+            const img1 = "Images/minion 3.jpg";
+            cy.get('#tabelBerkas > tbody > tr:nth-child(3) > td.text-center.berkas-list-item > div.decs-table > input[type=file]').attachFile(img1).wait(2000)
+
+            cy.get('div[id="fileList-filesyarat[10]"]').click({ force: true })
+            const img2 = "Images/minion 3.jpg";
+            cy.get('#tabelBerkas > tbody > tr:nth-child(6) > td.text-center.berkas-list-item > div.decs-table > input[type=file]').attachFile(img2).wait(2000)
+
+            var index04 = null
+            cy.get('.text-warning > select[id="pilihan[27]"]').select(data.almamater)
+            
+            // klik simpan 
+            cy.get('.btn-warning').click()
+
+            // cek keterangan modal 
+            cy.get('.bootbox-body').should('contain', 'Apakah anda yakin akan menyimpan data ini?')
+
+            // klik ok pada modal 
+            cy.get('.btn-primary').click()
+
+            // cek berhasil 
+            cy.get(':nth-child(1) > .alert').should('contain', 'Berhasil upload dokumen administrasi')
+        })
+
+
+    })
+})
