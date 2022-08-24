d0068642e7f83e27191d99ecc9f3f86b5c2b5967
Fikrimlyarf <fikrimlyarf@gmail.com>
2022-08-24 16:06:25 +0700

    [add] negatif test predikat

--- cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/05_pegawai/predikat_pegawai.cy.js
+++ cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/05_pegawai/predikat_pegawai.cy.js
@@ -69,8 +69,85 @@ describe('Testing data pekengkap predikat dosen', ()=>{
       .find('button.btn.btn-success.btn-xs.btn-flat').click()
     cy.get('.alert').should('contain', this.data.alertsimpan)
   });
-  it.only('', () => {
-    
+
+  //negatif test
+  it('Admin menambahkan data predikat dengan kode kosong', function () {
+    cy.get('#idperiode').select(this.data.periode).wait(0)
+    cy.get('#wrap-button > .btn-success').click()
+    // cy.get('#i_idpredikatdosen').type(this.data.kode05)
+    cy.get('#i_namapredikatdosen').type(this.data.predikat05)
+    cy.get('#i_namapredikatdosenen').type(this.data.predikaten05)
+    cy.get('#i_nilaibawah').type(this.data.nilaimin05)
+    cy.get('#i_nilaiatas').type(this.data.nilaimax05)
+      .parent()
+      .next()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.bootbox > .modal-dialog > .modal-content').should('contain', this.data.alertkosong)
   });
+  it('Admin menambahkan data predikat dengan nama predikat kosong', function () {
+    cy.get('#idperiode').select(this.data.periode).wait(0)
+    cy.get('#wrap-button > .btn-success').click()
+    cy.get('#i_idpredikatdosen').type(this.data.kode05)
+    // cy.get('#i_namapredikatdosen').type(this.data.predikat05)
+    cy.get('#i_namapredikatdosenen').type(this.data.predikaten05)
+    cy.get('#i_nilaibawah').type(this.data.nilaimin05)
+    cy.get('#i_nilaiatas').type(this.data.nilaimax05)
+      .parent()
+      .next()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.bootbox > .modal-dialog > .modal-content').should('contain', this.data.alertkosong)
+  });
+  it('Admin menambahkan data predikat dengan nama predikat EN kosong', function () {
+    cy.get('#idperiode').select(this.data.periode).wait(0)
+    cy.get('#wrap-button > .btn-success').click()
+    cy.get('#i_idpredikatdosen').type(this.data.kode05)
+    cy.get('#i_namapredikatdosen').type(this.data.predikat05)
+    // cy.get('#i_namapredikatdosenen').type(this.data.predikaten05)
+    cy.get('#i_nilaibawah').type(this.data.nilaimin05)
+    cy.get('#i_nilaiatas').type(this.data.nilaimax05)
+      .parent()
+      .next()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.bootbox > .modal-dialog > .modal-content').should('contain', this.data.alertkosong)
+  });
+  it('Admin menambahkan data predikat dengan nilai min kosong', function () {
+    cy.get('#idperiode').select(this.data.periode).wait(0)
+    cy.get('#wrap-button > .btn-success').click()
+    cy.get('#i_idpredikatdosen').type(this.data.kode05)
+    cy.get('#i_namapredikatdosen').type(this.data.predikat05)
+    cy.get('#i_namapredikatdosenen').type(this.data.predikaten05)
+    // cy.get('#i_nilaibawah').type(this.data.nilaimin05)
+    cy.get('#i_nilaiatas').type(this.data.nilaimax05)
+      .parent()
+      .next()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.bootbox > .modal-dialog > .modal-content').should('contain', this.data.alertkosong)
+  });
+  it.only('Admin menambahkan data predikat dengan nilai max kosong', function () {
+    cy.get('#idperiode').select(this.data.periode).wait(0)
+    cy.get('#wrap-button > .btn-success').click()
+    cy.get('#i_idpredikatdosen').type(this.data.kode05)
+    cy.get('#i_namapredikatdosen').type(this.data.predikat05)
+    cy.get('#i_namapredikatdosenen').type(this.data.predikaten05)
+    cy.get('#i_nilaibawah').type(this.data.nilaimin05)
+    // cy.get('#i_nilaiatas').type(this.data.nilaimax05)
+      .parent()
+      .next()
+      .next()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.bootbox > .modal-dialog > .modal-content').should('contain', this.data.alertkosong)
+  });
+  it('Admin menambahkan data predikat dengan kode lebih dari 5 digit', () => {});
+  it('Admin menambahkan data predikat dengan data yang sama', () => {});
+  it('Admin menambahkan data predikat dengan nilai min lebih besar dari nilai max', () => {});
+  it('Admin menambahkan data predikat dengan nilai min huruf', () => {});
+  it('Admin menambahkan data predikat dengan nilai min karakter', () => {});
+  it('Admin menambahkan data predikat dengan nilai max huruf', () => {});
+  it('Admin menambahkan data predikat dengan nilai max karakter', () => {});
+  it('Admin menambahkan data predikat dengan nilai min minus', () => {});
+  it('Admin menambahkan data predikat dengan nilai max minus', () => {});
+  it('Admin menambahkan data predikat dengan nilai min minus lebih dari 10 digit', () => {});
+  it('Admin menambahkan data predikat dengan nilai max minus lebih dari 10 digit', () => {});
+  it('Admin menambahkan data predikat dengan nilai min dan max diantara data yang sudah ada', () => {});
 
 });
