95c2e6ed7e8ca35bd64ec320fd2626b50adc60f8
Solechatul Imaniyah <nia.solecha@gmail.com>
2023-03-15 07:56:08 +0700

    dashboard_jenismatakuliah

--- cypress/e2e/Dapur/Nia/Dashboard.cy.js
+++ cypress/e2e/Dapur/Nia/Dashboard.cy.js
@@ -6,48 +6,46 @@ beforeEach("login",()=>{
   })
 
 it("Dashboard Mahasiswa Baru",()=>{
+    cy.visit('/siakad/home');
     cy.get(':nth-child(1) > .small-box-new > .inner > h3').invoke('text').then((jmlmhsbaru)=>{
-        cy.log(jmlmhsbaru);
-    })
-    cy.visit('/siakad/list_mahasiswa');
-    cy.get(':nth-child(2) > .box-header > .box-title > [data-testid]').click();
-    cy.get('#collapseAngkatan > .box-body > .slimScrollDiv > .tree > ul > :nth-child(1) > a').click();
-    cy.get('.col-md-4 > .callout').invoke('text').then((totalmhsbaru)=>{
-        cy.log(totalmhsbaru);
-        let splitText = totalmhsbaru.trim().replace('(','').split(" ")[2]
-        cy.log(splitText);
-        expect(splitText).to.equal('9');
+        const x=jmlmhsbaru.trim();
+
+        cy.visit('/siakad/list_mahasiswa');
+        cy.get(':nth-child(2) > .box-header > .box-title > [data-testid]').click();
+        cy.get('#collapseAngkatan > .box-body > .slimScrollDiv > .tree > ul > :nth-child(1) > a').click();
+        cy.get('.col-md-4 > .callout').invoke('text').then((totalmhsbaru)=>{
+            const y=totalmhsbaru.trim().replace('(','').split(" ")[2];
+            expect(y).to.equal(x);
+        })
     })
 })
 
 it("Dashboard Mahasiswa Aktif",()=>{
-    cy.visit('/siakad/home')
+    cy.visit('/siakad/home');
     cy.get(".small-box-new.bg-green h3:first").invoke('text').then((jmlmhsaktif)=>{
-        cy.log(jmlmhsaktif);
-    })
-    cy.visit('/siakad/list_mahasiswa');
-    cy.get(':nth-child(3) > .box-header > .box-title > [data-testid]').click();
-    cy.get('#collapseStatusMahasiswa > .box-body > .slimScrollDiv > .tree > ul > :nth-child(1) > a').click();
-    cy.get('.col-md-4 > .callout').invoke('text').then((totalmhsaktif)=>{
-        cy.log(totalmhsaktif);
-        let splitText = totalmhsaktif.trim().replace('(','').split(" ")[2]
-        cy.log(splitText);
-        expect(splitText).to.equal('13.334');
+        const x=jmlmhsaktif.trim();
+
+        cy.visit('/siakad/list_mahasiswa');
+        cy.get(':nth-child(3) > .box-header > .box-title > [data-testid]').click();
+        cy.get('#collapseStatusMahasiswa > .box-body > .slimScrollDiv > .tree > ul > :nth-child(1) > a').click();
+        cy.get('.col-md-4 > .callout').invoke('text').then((totalmhsaktif)=>{ 
+            cy.log(totalmhsaktif);
+            const y= totalmhsaktif.trim().replace('(','').split(" ")[2];
+            expect(y).to.equal(x);
+        })
     })
 })
 
-it.only("Dashboard Mahasiswa Terdaftar",()=>{
-    cy.visit('/siakad/home')
+it("Dashboard Mahasiswa Terdaftar",()=>{
+    cy.visit('/siakad/home');
     cy.get(".small-box-new.bg-green h3:last").invoke('text').then((jmlmhsterdaftar)=>{
-        cy.log(jmlmhsterdaftar);
-    })
-    cy.visit('/siakad/list_mahasiswa');
-    cy.get('.col-md-4 > .callout').invoke('text').then((totalmhsterdaftar)=>{
-        cy.log(totalmhsterdaftar);
-        cy.get('#collapseStatusMahasiswa > .box-body > .slimScrollDiv > .tree > ul > :nth-child(1) > a').click();
-        let splitText = totalmhsterdaftar.trim().replace('(','').split(" ")[2]
-        cy.log(splitText);
-        expect(splitText).to.equal('44.439');
+        const x=jmlmhsterdaftar.trim();
+        cy.log(x);
+        cy.visit('/siakad/list_mahasiswa');
+        cy.get('.col-md-4 > .callout').invoke('text').then((totalmhsterdaftar)=>{
+            const y = totalmhsterdaftar.trim().replace('(','').split(" ")[2];
+            expect(y).to.equal(x);
+        })
     })
 })
 
--- cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/02_perkuliahan/jenis_matakuliah/jenis_matakuliah.cy.js
+++ cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/02_perkuliahan/jenis_matakuliah/jenis_matakuliah.cy.js
@@ -14,18 +14,81 @@ describe('Automation Jenis Mata Kuliah', ()=>{
     cy.get('.content-header > h1').should('contain','Jenis Mata Kuliah');
   });
 
-  data.tambah.forEach((test)=>{
+  /*data.tambah.forEach((test)=>{
     it('Tambah data', ()=>{
       cy.get('#wrap-button > .btn').click();
       cy.get('#i_idjenismk').type(test.kode_jenismk);
       cy.get('#i_namajenismk').type(test.nama_jenismk);
       cy.get(':nth-child(3) > .btn-success > .fa').click();
     })
+  })*/
+
+  it('Admin bisa menambah data',()=>{
+    //positif case
+      cy.get('#wrap-button > .btn').click();
+      cy.get('#i_idjenismk').type('A1');
+      cy.get('#i_namajenismk').type('Test1');
+      cy.get(':nth-child(3) > .btn-success > .fa').click();
+
+      cy.get('#wrap-button > .btn').click();
+      cy.get('#i_idjenismk').clear();
+      cy.get('#i_namajenismk').clear();
+      cy.get('#i_idjenismk').type('A/');
+      cy.get('#i_namajenismk').type('Test-');
+      cy.get(':nth-child(3) > .btn-success > .fa').click();
+
+    //Negatif Case
+    //Kolom kode dan nama kosong
+      cy.get('#wrap-button > .btn').click();
+      cy.get('#i_idjenismk').clear();
+      cy.get('#i_namajenismk').clear();
+      cy.get(':nth-child(3) > .btn-success > .fa').click();
+      cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah').click();
+      cy.get('.modal-footer > .btn').click();
+
+    //Kolom nama kosong
+      cy.get('#wrap-button > .btn').click();
+      cy.get('#i_idjenismk').clear();
+      cy.get('#i_namajenismk').clear();
+      cy.get('#i_idjenismk').type('A3');
+      cy.get(':nth-child(3) > .btn-success > .fa').click();
+      cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah').click();
+      cy.get('.modal-footer > .btn').click();
+
+    //Kolom kode kosong
+    cy.get('#wrap-button > .btn').click();
+    cy.get('#i_idjenismk').clear();
+    cy.get('#i_namajenismk').clear();
+    cy.get('#i_namajenismk').type('Kampus Merdeka');
+    cy.get(':nth-child(3) > .btn-success > .fa').click();
+    cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah').click();
+    cy.get('.modal-footer > .btn').click();
+
+  });
+
+  it('Admin bisa mengubah data',()=>{
+      cy.get('td').contains('A1').next().parent().find('.btn.btn-warning.btn-xs.btn-flat').click();
+      cy.get('#u_idjenismk').clear().type('A2').parent().next().next().find('.btn.btn-success.btn-xs.btn-flat').click();
+      cy.get('.alert').should('contain','Pengubahan data jenis mata kuliah berhasil');
   })
 
+  it('Admin bisa menghapus data',()=>{
+    cy.get('td').contains('A/').next().parent().find('.btn.btn-danger.btn-xs.btn-flat').click();
+    cy.get('#modal-konfirmasi').contains('Apakah anda yakin akan menghapus data ini?');
+    cy.get('.btn.btn-primary').click();
+    cy.get('.alert').should('contain','Penghapusan data jenis mata kuliah berhasil');
+
+    //Negatif case
+    //Hapus jenis mata kuliah yang tereferensi dengan data lain
+    cy.get('td').contains('A').next().parent().find('.btn.btn-danger.btn-xs.btn-flat').click();
+    cy.get('#modal-konfirmasi').contains('Apakah anda yakin akan menghapus data ini?');
+    cy.get('.btn.btn-primary').click();
+    cy.get('.alert').should('contain','Penghapusan data jenis mata kuliah gagal, data masih dijadikan referensi pada Mata Kuliah');
+    cy.get('.close').click();
+  })
   //row function private
   //function public
-  it.only('Ubah data',  ()=>{
+  /*it('Ubah data',  ()=>{
     cy.get('table.table.table-bordered.table-striped.dataTable > tbody > tr > td:nth-child(1)').each(($el)=>{
       //looping table tidak bisa pakai fixture array
       if ($el.text() === data.kode) {
@@ -35,5 +98,5 @@ describe('Automation Jenis Mata Kuliah', ()=>{
       }
       // cy.log($el.text())
     })
-  })
+  })*/
 });
