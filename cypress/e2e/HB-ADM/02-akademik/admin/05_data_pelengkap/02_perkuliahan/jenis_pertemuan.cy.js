b484d4a0cdec11534ade7f75706c7cb16fc0a3b5
Solechatul Imaniyah <nia.solecha@gmail.com>
2023-03-28 08:43:47 +0700

    master_jenispertemuan_statushadir

--- cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/02_perkuliahan/jenis_pertemuan.cy.js
+++ cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/02_perkuliahan/jenis_pertemuan.cy.js
@@ -1,20 +1,141 @@
 /// <reference types="cypress"/>
-
-const namaMenu="Jenis Pertemuan";
-
-describe(namaMenu, ()=>{
+ 
+ 
+ 
+describe('Automation Jenis Pertemuan', ()=>{
  
   beforeEach(() => {
-    //login
-      cy.loginsuperadmin()
-    //open Akademik module
-      cy.modulakademik()
-    //go to target page
-      cy.menujenispertemuan()
+    cy.login();
+    cy.modulakademik();
+    cy.visit('/siakad/ms_jenispertemuan');
   });
-
-  it('Buka Halaman', () => {
-    cy.get('.content-header > h1').should('contain',namaMenu)
+ 
+  it('Admin bisa menambah data', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idjenispertemuan').type('Test')
+    cy.get('#i_namajenispertemuan').type('Data Testing')
+    cy.get('#i_namasingkatjenispertemuan').type('Testing')
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data jenis pertemuan berhasil')
   });
-
+ 
+  it('Admin tidak mengisi kolom Kode', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_namajenispertemuan').type('Data Testing')
+    cy.get('#i_namasingkatjenispertemuan').type('Testing')
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah')
+    cy.get('.modal-footer > .btn').click()
+  });
+  it('Admin tidak mengisi kolom Nama Jenis Pertemuan', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idjenispertemuan').type('Test')
+    cy.get('#i_namasingkatjenispertemuan').type('Testing')
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah')
+    cy.get('.modal-footer > .btn').click()
+  });
+  it('Admin tidak mengisi kolom Nama Singkat Jenis Pertemuan', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idjenispertemuan').type('Test')
+    cy.get('#i_namajenispertemuan').type('Data Testing')
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah')
+    cy.get('.modal-footer > .btn').click()
+  });
+  it('Admin mencentang Presensi', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idjenispertemuan').type('Test2')
+    cy.get('#i_namajenispertemuan').type('Data Testing2')
+    cy.get('#i_namasingkatjenispertemuan').type('Testing')
+    cy.get(':nth-child(4) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data jenis pertemuan berhasil')
+  });
+  it('Admin mencentang Ujian', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idjenispertemuan').type('Test3')
+    cy.get('#i_namajenispertemuan').type('Data Testing3')
+    cy.get('#i_namasingkatjenispertemuan').type('Testing')
+    cy.get(':nth-child(5) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data jenis pertemuan berhasil')
+  });
+  it('Admin mencentang Presensi & Ujian', () => {
+  cy.get('#wrap-button > .btn').click()
+  cy.get('#i_idjenispertemuan').type('Test4')
+  cy.get('#i_namajenispertemuan').type('Data Testing4')
+  cy.get('#i_namasingkatjenispertemuan').type('Testing')
+  cy.get(':nth-child(4) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+  cy.get(':nth-child(5) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+    .parent()
+    .parent()
+    .parent()
+    .parent()
+    .find('button.btn.btn-success.btn-xs.btn-flat').click()
+  cy.get('.alert').should('contain','Penambahan data jenis pertemuan berhasil')
+  });
+  it('Admin memilih jenis yang sudah digunakan', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idjenispertemuan').type('Test5')
+    cy.get('#i_namajenispertemuan').type('Data Testing5')
+    cy.get('#i_namasingkatjenispertemuan').type('Testing')
+    cy.get('#i_jenis').select('Kuliah')
+      .parent()
+      .next()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.text-danger').should('contain', ' Jenis : kuliah sudah digunakan')
+  });
+  it('Admin bisa mengubah data', () => {
+    cy.get('.table>tbody>tr>td:nth-child(2)').each(($el,index,$list)=>{
+      if($el.text()==='Data Testing'){ 
+        cy.get($el).parent().find('button.btn.btn-warning.btn-xs.btn-flat').click();
+      }
+    })
+    cy.get('#u_namajenispertemuan').clear().type('Ubah Data Testing')
+      .parent() 
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Pengubahan data jenis pertemuan berhasil')
+  });
+  it('Admin batal menghapus data', () => {
+    cy.get('.table>tbody>tr>td:nth-child(2)').each(($el,index,$list)=>{
+      if($el.text()==='Ubah Data Testing'){ 
+        cy.get($el).parent().find('button.btn.btn-danger.btn-xs.btn-flat').click();
+      }
+    })
+    cy.get('#modal-konfirmasi').should('contain', 'Apakah anda yakin akan menghapus data ini?')
+    cy.get('.modal-footer > .btn-default').click()
+ 
+    // cy.get('.alert').should('contain','Pengubahan data jenis pertemuan berhasil')
+  });
+  it('Admin bisa menghapus data', () => {
+    cy.get('.table>tbody>tr>td:nth-child(2)').each(($el,index,$list)=>{
+      if($el.text()==='Ubah Data Testing'){ 
+        cy.get($el).parent().find('button.btn.btn-danger.btn-xs.btn-flat').click();
+      }
+    })
+    cy.get('#modal-konfirmasi').should('contain', 'Apakah anda yakin akan menghapus data ini?')
+    cy.get('.btn-primary').click()
+ 
+    cy.get('.alert').should('contain','Penghapusan data jenis pertemuan berhasil')
+  });
+ 
 });
+ 
--- cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/02_perkuliahan/status_hadir.cy.js
+++ cypress/e2e/HB-ADM/02-akademik/admin/05_data_pelengkap/02_perkuliahan/status_hadir.cy.js
@@ -1,20 +1,103 @@
 /// <reference types="cypress"/>
-
-const namaMenu="Status Hadir";
-
-describe(namaMenu, ()=>{
+ 
+describe('Automation Status Hadir', ()=>{
  
   beforeEach(() => {
-    //login
-      cy.loginsuperadmin()
-    //open Akademik module
-      cy.modulakademik()
-    //go to target page
-      cy.menustatushadir()
+    cy.login();
+    cy.modulakademik();
+    cy.visit('/siakad/ms_statushadir');
   });
-
-  it('Buka Halaman', () => {
-    cy.get('.content-header > h1').should('contain',namaMenu)
+ 
+  it('Admin bisa menambah data', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idstatushadir').type('T')
+    cy.get('#i_namastatushadir').type('Testing')
+      .parent() 
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data status hadir berhasil')
   });
-
+  it('Admin tidak mengisi kolom Kode', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_namastatushadir').type('Testing')
+      .parent() 
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah')
+    cy.get('.modal-footer > .btn').click()
+  });
+  it('Admin tidak mengisi kolom Nama Status Hadir', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idstatushadir').type('T')
+      .parent() 
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('#modal-konfirmasi').should('be.visible').and('contain', 'Mohon mengisi isian yang bergaris merah')
+    cy.get('.modal-footer > .btn').click()
+  });
+  it('Admin mencentang Hadir', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idstatushadir').type('U')
+    cy.get('#i_namastatushadir').type('Testing 1')
+    cy.get(':nth-child(3) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+      .parent() 
+      .parent()
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data status hadir berhasil')
+  });
+  it('Admin mencentang Dosen', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idstatushadir').type('V')
+    cy.get('#i_namastatushadir').type('Testing 2')
+    cy.get(':nth-child(4) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+      .parent() 
+      .parent()
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data status hadir berhasil')
+  });
+  it('Admin mencentang Mahasiswa', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idstatushadir').type('W')
+    cy.get('#i_namastatushadir').type('Testing 3')
+    cy.get(':nth-child(5) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+      .parent() 
+      .parent()
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data status hadir berhasil')
+  });
+  it('Admin mencentang Hadir, Dosen & Mahasiswa', () => {
+    cy.get('#wrap-button > .btn').click()
+    cy.get('#i_idstatushadir').type('X')
+    cy.get('#i_namastatushadir').type('Testing 4')
+    cy.get(':nth-child(3) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+    cy.get(':nth-child(4) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+    cy.get(':nth-child(5) > .labelinput > .icheckbox_minimal > .iCheck-helper').click()
+      .parent() 
+      .parent()
+      .parent()
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Penambahan data status hadir berhasil')
+  });
+  it('Admin bisa mengubah data', () => {
+    cy.get('.table>tbody>tr>td:nth-child(2)').each(($el,index,$list)=>{
+      if($el.text()==='Testing 1'){ 
+        cy.get($el).parent().find('button.btn.btn-warning.btn-xs.btn-flat').click();
+      }
+    })
+    cy.get('#u_namastatushadir').clear().type('Ubah Data testing')
+    .parent() 
+      .parent()
+      .find('button.btn.btn-success.btn-xs.btn-flat').click()
+    cy.get('.alert').should('contain','Pengubahan data status hadir berhasil')
+  });
+ 
+ 
 });
+ 
